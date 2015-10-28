3c1f82d8d6aaa67e498024703fb3df97bcd66e45
Nina Pavlich <nina@ninalp.com>
2015-10-28 13:50:23 -0400

    update liscense, manifest and setup for single-line installation

--- setup.py
+++ setup.py
@@ -2,19 +2,25 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django_batch_uploader',
-    packages = ['django_batch_uploader'],
-    version = '0.8',
-    description = 'Batch Uploading Mechanism for Django Admin',
+    version = '0.9',
     author = 'Nina Pavlich',
     author_email='nina@ninalp.com',
     url = 'https://github.com/ninapavlich/django-batch-uploader',
+    license = "MIT",
+
+    description = 'Batch Uploading Mechanism for Django Admin',
     keywords = ['libraries', 'web development'],
+
+    include_package_data = True,
+    packages = ['django_batch_uploader'],
+    
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python'
     ]
